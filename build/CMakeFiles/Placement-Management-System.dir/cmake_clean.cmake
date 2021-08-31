f1913dbc6c9e593268ff283bf099ba5e6e086156
Aditya Kshirsagar <kaditya.anil@gmail.com>
2021-08-31 10:42:27 +0530

    now using cmake to build the program

---
+++ build/CMakeFiles/Placement-Management-System.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Placement-Management-System.dir/main.cpp.o"
+  "CMakeFiles/Placement-Management-System.dir/main.cpp.o.d"
+  "Placement-Management-System"
+  "Placement-Management-System.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Placement-Management-System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
